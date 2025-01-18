908eaaef1af83dbb12b0b2e76257ec948002dbd4
aungmoe32 <aungmoemyintthu@gmail.com>
2025-01-18 21:02:29 +0630

    add chat list item

---
+++ resources/views/components/chat/chat-item.blade.php
@@ -0,0 +1,32 @@
+@props([
+    'active' => false,
+    'name' => 'User',
+])
+<div class="px-3 flex items-center  cursor-pointer hover:bg-white/10 {{ $active ? 'bg-white/10' : 'bg-white' }}">
+    <div>
+        <img class="h-12 w-12 rounded-full"
+            src="https://darrenjameseeley.files.wordpress.com/2014/09/expendables3.jpeg" />
+    </div>
+    <div class="ml-4 flex-1 border-b border-grey-lighter py-4">
+        <div class="flex items-bottom justify-between">
+            <p class="text-grey-darkest">
+                {{ $name }}
+            </p>
+            <p class="text-xs text-grey-darkest">
+                12:45 pm
+            </p>
+        </div>
+        <p class="flex items-start space-x-2  text-grey-dark mt-1 text-sm text-gray-500">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-check2-all w-4 text-blue-500"
+                viewBox="0 0 16 16">
+                <path
+                    d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z" />
+                <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z" />
+            </svg>
+            <span>
+                how are you
+            </span>
+        </p>
+    </div>
+    <x-chat.dropdown></x-chat-dropdown>
+</div>
---
+++ resources/views/components/chat/dropdown.blade.php
@@ -0,0 +1,53 @@
+<x-dropdown align="right" width="48">
+    <x-slot name="trigger">
+        <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                class="bi bi-three-dots-vertical w-4 text-gray-700" viewBox="0 0 16 16">
+                <path
+                    d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+            </svg>
+
+
+        </button>
+    </x-slot>
+
+    <x-slot name="content">
+
+        <div class="w-full p-1">
+
+            <button
+                class="items-center gap-3 flex w-full px-4 py-2 text-left text-sm leading-5 text-gray-500 hover:bg-gray-100 transition-all duration-150 ease-in-out focus:outline-none focus:bg-gray-100">
+
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                        <path fill-rule="evenodd"
+                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                    </svg>
+                </span>
+
+                View Profile
+
+            </button>
+            <button
+                class="items-center gap-3 flex w-full px-4 py-2 text-left text-sm leading-5 text-gray-500 hover:bg-gray-100 transition-all duration-150 ease-in-out focus:outline-none focus:bg-gray-100">
+
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-trash-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                    </svg>
+                </span>
+
+                Delete
+
+            </button>
+
+
+
+        </div>
+
+    </x-slot>
+</x-dropdown>
