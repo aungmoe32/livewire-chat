829abc71e79e5d0ad6474654eec9325f8736ff50
aungmoe32 <aungmoemyintthu@gmail.com>
2025-01-19 21:31:45 +0630

    add users list ui

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Users extends Component
 {
+
+    public function message($userId)
+    {
+        dd($userId);
+    }
+
     public function render()
     {
-        return view('livewire.users');
+        return view('livewire.users', [
+            'users' => User::all()
+        ]);
     }
 }
