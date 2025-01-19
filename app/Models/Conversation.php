9363710abb87e6306044db61a529da08773853a3
aungmoe32 <aungmoemyintthu@gmail.com>
2025-01-19 21:06:05 +0630

    add message, conversation relationships

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Conversation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'receiver_id',
+        'sender_id'
+    ];
+
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
